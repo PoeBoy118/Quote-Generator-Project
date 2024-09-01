2dbe0ef95c5c6aca6fbbbca44c15c53b35e189b9
PoeBoy118 <elishagarris@gmail.com>
2024-09-01 02:24:21 -0400

    First Commit

---
+++ scripts/generatorScript.js
@@ -0,0 +1,13 @@
+import { nounList } from "./wordLibrary"
+
+const testButton = document.getElementById("testButton")
+
+let buttonTest = () => {
+    console.log("Button Works")
+}
+
+let generator = () => {
+    nounList = nounList
+}
+
+generator()
---
+++ scripts/wordLibrary.js
@@ -0,0 +1,5 @@
+let nounList = ["Bob", "Cat", "Burger", "Flash Kick"]
+let verbList = ["kick", "eat", "throw", "parry"]
+let adjList = ["bodaciously", "radically", "sadly", "enthusiastically"]
+
+export {nounList, verbList, adjList}
